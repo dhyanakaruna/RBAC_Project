74ceaeb511acc3877aabb2aa6a6dd769efebdb19
Dhyana <55345793+dhyanakaruna@users.noreply.github.com>
2024-12-14 23:22:55 +0530

    Removed few changes

--- rbac_project/rbac/utils.py
+++
@@ -1,9 +0,0 @@
-from .models import AuditLog
-
-def log_access_attempt(user, action, resource, outcome):
-    AuditLog.objects.create(
-        user=user,
-        action=action,
-        resource=resource,
-        outcome=outcome
-    )
